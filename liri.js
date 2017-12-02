f4333810766221f1d4d7683430c6e2e64f571807
Jonathan Landrey Smith <jonathansmith@jonathans-mbp.uconnect.utah.edu>
2017-12-02 09:12:15 -0700

    file

--- liri.js
+++ liri.js
@@ -67,7 +67,8 @@ var spotifyThisSong = function() {
             name: "songTitle"
         }
     ]).then(function(response) {
-        console.log("Searching for: " + response.songTitle);
+        var songTitle = response.songTitle;
+        console.log("Searching for: " + songTitle);
 
         var spotify = new Spotify({
             id: keys.client_id,
@@ -77,44 +78,27 @@ var spotifyThisSong = function() {
         spotify.search(
             { 
                 type: 'track', 
-                query: response.songTitle, 
+                query: songTitle, 
                 limit: "1"
             }
         ).then(function(response) {
-            var artist = response.tracks.items[0].artists[0].name;
-            var name = response.tracks.items[0].name;
-            var previewURL = response.tracks.items[0].preview_url;
-            var album = response.tracks.items[0].album.name;
-            console.log("Artist: " + artist + "\nName: " + name + "\nPreview: " + previewURL + "\nAlbum: " + album);
+            if (songTitle == response.tracks.items[0].name) {
+                var artist = response.tracks.items[0].artists[0].name;
+                var name = response.tracks.items[0].name;
+                var previewURL = response.tracks.items[0].preview_url;
+                var album = response.tracks.items[0].album.name;
+                console.log("Artist: " + artist + "\nName: " + name + "\nPreview: " + previewURL + "\nAlbum: " + album);
+            } else {
+                console.log("Sorry, we could not find " + songTitle);
+                console.log("Here is one of our favorites!");
+                console.log("Artist: Ace of Base" + "\nName: The Sign" + "\nPreview: ..."+ "\nAlbum: The Sign")
+            }
+            
             reset();                    
         }).catch(function(err) {
             console.log(err);
         });
     });
-//    * This will show the following information about the song in your terminal/bash window
-     
-//      * Artist(s)
-     
-//      * The song's name
-     
-//      * A preview link of the song from Spotify
-     
-//      * The album that the song is from
-
-//    * If no song is provided then your program will default to "The Sign" by Ace of Base.
-   
-//    * You will utilize the [node-spotify-api](https://www.npmjs.com/package/node-spotify-api) package in order to retrieve song information from the Spotify API.
-   
-//    * Like the Twitter API, the Spotify API requires you sign up as a developer to generate the necessary credentials. You can follow these steps in order to generate a **client id** and **client secret**:
-
-//    * Step One: Visit <https://developer.spotify.com/my-applications/#!/>
-   
-//    * Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
-
-//    * Step Three: Once logged in, navigate to <https://developer.spotify.com/my-applications/#!/applications/create> to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
-
-//    * Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the [node-spotify-api package](https://www.npmjs.com/package/node-spotify-api). See the 
-
 }
 
 var movieThis = function() {
